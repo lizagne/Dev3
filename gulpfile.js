e2c04e35a36e29d245392565f2587aae5eb1d58e
Liz Verde <>
2017-11-02 14:32:21 +0000

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,18 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
 let sass = require('gulp-sass');
-let about = require('gulp-about');
 let watch = require('gulp-watch');
-let refresh = require('gulp-refresh')
-let gulpSequence = require('gulp-sequence');
+let gulpSequence = require('gulp-sequence')
+let browserSync = require('browser-sync').create();
 
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
 
 gulp.task('sass', function () {
     var stream = gulp.src('./scss/styles.scss')
@@ -17,34 +24,27 @@ gulp.task('sass', function () {
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src('css/styles.css')
-	.pipe(cleanCSS({compatibility: 'ie8'}))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(gulp.dest('./css/'));
+    return gulp.src('css/styles.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./css/'));
 });
 
-gulp.task('styles', function(callback){
-	gulpSequence('sass', 'minify-css')(callback)
+gulp.task('rebuild-everything', function(callback){
+    gulpSequence('sass', 'minify-css')(callback)
 });
 
-
-gulp.task('scss', () => {
-  gulp
-    .src('src/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('dist'))//Liz added this for gulp-about
-    .pipe(refresh())
-})
+gulp.task('rebuild-then-reload', ['rebuild-everything'], function (done) {
+    browserSync.reload();
+    done();
+});
 
 gulp.task('watch', function () {
-    refresh.listen() //Liz added this line to help gulp refresh work
-	gulp.watch('./scss/*.scss', ['styles']);
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch(['./scss/*.scss', './*.html', './*.js'], ['rebuild-then-reload']);
 });
-
-gulp.task('about', function () {
-    return gulp.src('package.json')
-        .pipe(about())
-        .pipe(gulp.dest('dist'));  // writes dist/about.json 
-});
-
-
